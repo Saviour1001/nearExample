884fa6155f8d61ddad821aa6ce974dbfac7d25cd
Saviour1001 <harshghodkar@gmail.com>
2022-09-25 22:24:08 +0530

    added more functions to near

--- src/nearRPC.ts
+++ src/nearRPC.ts
@@ -1,6 +1,6 @@
 import { KeyStore } from "near-api-js/lib/key_stores";
 import { SafeEventEmitterProvider } from "@web3auth/base";
-import { KeyPair } from "near-api-js";
+import { connect, KeyPair, keyStores } from "near-api-js";
 import { hex2buf } from "@taquito/utils";
 import { base_encode } from "near-api-js/lib/utils/serialize";
 export default class NearRPC {
@@ -10,6 +10,24 @@ export default class NearRPC {
     this.provider = provider;
   }
 
+  makeConnection = async () => {
+    const myKeyStore = new keyStores.InMemoryKeyStore();
+    const keyPair = await this.getNearKeyPair();
+    await myKeyStore.setKey("testnet", "example-account.testnet", keyPair);
+
+    // connections can be made to any network
+    // refer https://docs.near.org/tools/near-api-js/quick-reference#connect
+
+    const connectionConfig = {
+      networkId: "testnet",
+      keyStore: myKeyStore, // first create a key store
+      nodeUrl: "https://rpc.testnet.near.org",
+      walletUrl: "https://wallet.testnet.near.org",
+      helperUrl: "https://helper.testnet.near.org",
+      explorerUrl: "https://explorer.testnet.near.org",
+    };
+    const nearConnection = await connect(connectionConfig);
+  };
   getNearKeyPair = async (): Promise<any> => {
     try {
       const privateKey = (await this.provider.request({
@@ -20,6 +38,26 @@ export default class NearRPC {
     } catch (error) {
       console.error(error);
       return null;
+    }
+  };
+
+  getAccounts = async () => {
+    try {
+      const keyPair = await this.getNearKeyPair();
+      return keyPair?.getPublicKey().toString();
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  getBalance = async () => {
+    try {
+      const keyPair = await this.getNearKeyPair();
+      // keyPair.pkh is the account address.
+      // const balance =
+      // return balance;
+    } catch (error) {
+      return error;
     }
   };
 }
